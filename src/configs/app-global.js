d14b16dcaac64ccce9d5b279778e5ac831f0994e
kolluUmasree <umasree@ensarsolutions.com>
2025-03-26 12:01:01 +0530

    first commit

---
+++ src/configs/app-global.js
@@ -0,0 +1,36 @@
+export const PROJECT_NAME = 'Foodyman marketplace';
+export const BASE_URL = 'https://single-api.foodyman.org';
+export const api_url = BASE_URL + '/api/v1/';
+export const api_url_admin_dashboard = BASE_URL + '/api/v1/dashboard/';
+export const IMG_URL = '';
+export const MAP_API_KEY = 'AIzaSyDfe-B3bRqFV6yNU3t7rhMR4Nsm_kzsaf4';
+export const export_url = BASE_URL + '/storage/';
+export const example = BASE_URL + '/';
+export const defaultCenter = { lat: 40.7127281, lng: -74.0060152 };
+
+export const VAPID_KEY =
+  'BPF2UBmQ7aS14ryUSFFG0PNNYBaLlCvmsd2RHG6ndaHE6aL-_blBxQoK0KAohAS8tkbbdV_6R5RPmUWkJCVZXCM';
+export const API_KEY = 'AIzaSyASPzFJ1nz8PAlKpda3PueDGAkzsxefUrs';
+export const AUTH_DOMAIN = 'goshop-70695.firebaseapp.com';
+export const PROJECT_ID = 'goshop-70695';
+export const STORAGE_BUCKET = 'goshop-70695.firebasestorage.app';
+export const MESSAGING_SENDER_ID = '30741596190';
+export const APP_ID = '1:30741596190:web:d5367e5b6898defa72460f';
+export const MEASUREMENT_ID = 'G-8YZSFPQB4F';
+
+export const RECAPTCHASITEKEY = '6LesdI0mAAAAAOsL4S5ZsqGmaW5VTw6z8bc7_maZ';
+
+export const DEMO_SELLER = 107; // seller_id
+export const DEMO_SHOP = 501; // seller_id
+export const DEMO_DELIVERYMAN = 106; // deliveryman_id
+export const DEMO_MANEGER = 114; // maneger_id
+export const DEMO_MODERATOR = 297; // moderator_id
+export const DEMO_ADMIN = 501; // administrator_id
+
+export const SUPPORTED_FORMATS = [
+  'image/jpg',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'image/svg',
+];
